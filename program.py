54337145a116a9c897abf1543d4fd7148138ce19
rwooley159 <stevewolfie@gmail.com>
2014-06-24 11:33:59 -0700

    Fixed error handler

--- program.py
+++ program.py
@@ -30,7 +30,8 @@ def CheckMachineState():
 	conn.close()
 	
 def actonbarcode(barcode):
-	if len(barcode) == 12:
+	try:
+		if len(barcode) == 12:
 			lcd.clear()
 			lcd.message("Lot Roll Number:\n" + barcode)
 			sleep(3)
@@ -43,22 +44,41 @@ def actonbarcode(barcode):
 			conn.close()
 			sleep(2)
 			lcd.backlight(Adafruit_CharLCDPlate.TEAL)
+			
+		if len(barcode) == 7:
+			lcd.clear()
+			lcd.message("SKU:\n" + barcode)
+			sleep(3)
+			conn = pyodbc.connect("DSN=WAGODB;UID=sa;PWD=moldex")
+			cursor = conn.execute("{call dbo.SKU(? , ?)}", machineid, barcode )
+			conn.commit()
+			lcd.clear()
+			lcd.backlight(Adafruit_CharLCDPlate.GREEN)
+			lcd.message("SUCCESS!\nSKU Updated")			
+			conn.close()
+			sleep(2)
+			lcd.backlight(Adafruit_CharLCDPlate.TEAL)	
+			
+	except Exception as e:
+		errorHandler(e)
+		pass
 	
 
 def errorHandler(error):
-	#conn = pyodbc.connect("DSN=WAGODB;UID=sa;PWD=moldex")
-	#cursor = conn.execute("{call dbo.LogError(?, ?)}", machineid, str(error))
-	#conn.commit()
-	#conn.close()
-	#for x in range(0,4):
-	#	lcd.clear()
-	#	lcd.backlight(0x01)
-	#	lcd.message("     ERROR")
-	#	sleep(.5)
-	#	lcd.clear()
-	#	lcd.message("\n     ERROR")
-	#	sleep(.5)		
-	print "WTF"	
+	conn = pyodbc.connect("DSN=WAGODB;UID=sa;PWD=moldex")
+	cursor = conn.execute("{call dbo.LogError(?, ?)}", machineid, str(error))
+	conn.commit()
+	conn.close()
+	for x in range(0,4):
+		lcd.clear()
+		lcd.backlight(0x01)
+		lcd.message("     ERROR")
+		sleep(.5)
+		lcd.clear()
+		lcd.message("\n     ERROR")
+		sleep(.5)	
+		
+	
 
 	
 	
@@ -70,8 +90,7 @@ def mainloop():
 			actonbarcode(barcode)
 		except Exception as e:
 			errorHandler(e)
-			pass
-			
+			pass		
 			
 
 
